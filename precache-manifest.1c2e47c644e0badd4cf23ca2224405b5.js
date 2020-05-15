fc509dd497eb05a9b3324552f477d2cbd3ca0800
Alex <grok88@tut.by>
2020-05-15 19:34:36 +0300

    Updates

---
+++ precache-manifest.1c2e47c644e0badd4cf23ca2224405b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e0c2db27c812ced6c2577b896be29c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "6217dd24b9e9ddacb646",
+    "url": "/Portfolio/static/css/main.210564f9.chunk.css"
+  },
+  {
+    "revision": "3bca0a3ca8ba272115d5",
+    "url": "/Portfolio/static/js/2.0209b9a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.0209b9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6217dd24b9e9ddacb646",
+    "url": "/Portfolio/static/js/main.1e4fb4c2.chunk.js"
+  },
+  {
+    "revision": "07504943266392ed439e",
+    "url": "/Portfolio/static/js/runtime-main.e4f7e474.js"
+  }
+]);
