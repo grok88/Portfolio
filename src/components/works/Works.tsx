787d77210697a0ee09d4e17f4c3c9c8dc264a16e
Alex <grok88@tut.by>
2020-09-13 14:36:17 +0300

    add sass

--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 import style from './Works.module.css';
 import styleContainer from '../common/styles/Container.module.css';
-import Work from "./work/Work";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faLaptopCode, faBriefcase} from '@fortawesome/free-solid-svg-icons'
-import Title from "../common/components/title/Title";
-
+import Work from './work/Work';
+import {faBriefcase} from '@fortawesome/free-solid-svg-icons'
+import Title from '../common/components/title/Title';
+import social from  './../../assets/images/social.jpg'
+import todolist from  './../../assets/images/todolist.jpg'
+import plug from  './../../assets/images/plug.jpg'
 
 
 const Works = () => {
@@ -15,10 +16,10 @@ const Works = () => {
             <div className={`${styleContainer.container} ${style.worksContainer}`}>
               <Title title={'Portfolio.'}  icon={faBriefcase}/>
                 <div className={style.works}>
-                    <Work title={'Social network'} description={'Lorem ipsum dolor sit amet.'}/>
-                    <Work title={'TodoList'} description={'Lorem ipsum dolor sit amet.'}/>
-                    <Work title={'Work 3'} description={'Lorem ipsum dolor sit amet.'}/>
-                    <Work title={'Work 4'} description={'Lorem ipsum dolor sit amet.'}/>
+                    <Work title={'Social network'} description={'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'} imageUrl={social}/>
+                    <Work title={'TodoList'} description={'Lorem ipsum dolor sit amet.'} imageUrl={todolist}/>
+                    <Work title={'Work 3'} description={'Lorem ipsum dolor sit amet.'} imageUrl={plug}/>
+                    <Work title={'Work 4'} description={'Lorem ipsum dolor sit amet.'} imageUrl={plug}/>
                 </div>
             </div>
         </div>
--- src/components/works/work/Work.tsx
+++ src/components/works/work/Work.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import style from './Work.module.css';
+import React from 'react';
+import style from './Work.module.scss';
 
 type PropsType = {
-    title: string,
-    description: string
+    title: string;
+    description: string;
+    imageUrl:string;
 }
 
 const Work = (props: PropsType) => {
-    const {title, description} = props;
+    const {title, description,imageUrl} = props;
     return (
         <div className={style.work}>
-            <div className={style.imageBlock}>
-                <div className={style.img}></div>
-                <button>Смотреть</button>
-            </div>
-            <h3>{title}</h3>
-            <p>{description}</p>
+            <a href="#" className={style.workItem}>
+                <div className={style.workImage}>
+                    <img src={imageUrl} alt="#"/>
+                </div>
+                <div className={style.workContent}>
+                    <span className={style.cat}>{title}</span>
+                    <h4 className={style.title}>Best Way to Design </h4>
+                    <div className={style.workDescription}>{description}</div>
+                </div>
+            </a>
         </div>
     );
 }
