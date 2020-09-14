5ea4899c43cee7464abb542e66a3a987eac75746
Alex <grok88@tut.by>
2020-09-14 11:02:25 +0300

    Add formik to contact form

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,33 +1,91 @@
-import React from "react";
+import React from 'react';
 import style from './Contact.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
-import Title from "../common/components/title/Title";
-import { faClipboard, faSoap, faArchway} from '@fortawesome/free-solid-svg-icons'
+import Title from '../common/components/title/Title';
+import {faArchway, faClipboard} from '@fortawesome/free-solid-svg-icons';
+import {useFormik} from 'formik';
 
-import { FontAwesomeIcon , } from "@fortawesome/react-fontawesome";
+
+import {FontAwesomeIcon,} from "@fortawesome/react-fontawesome";
+
+type FormValuesType = {
+    name: string;
+    email: string;
+    text: string;
+}
+type LoginErrorType = {
+    name?: string;
+    email?: string;
+    text?: string;
+}
+
+const validate = (values: FormValuesType) => {
+    const errors: LoginErrorType = {};
+    if (!values.name) {
+        errors.name = 'Required';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    if (!values.text) {
+        errors.text = 'Required';
+    }
+
+
+    return errors;
+};
 
 const Contact = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            text: ''
+        },
+        validate,
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+            formik.resetForm();
+            // resetForm({name:''});
+        },
+    });
+
     return (
         <div className={style.contactBlock}>
             <div className={`${styleContainer.container} ${style.contactContainer}`}>
                 <Title title={'Contact Me.'} icon={faClipboard}/>
                 <div className={style.contactFormItem}>
                     <h4 className={style.contactTitle}>get in touch</h4>
-                    <form  className={style.contactForm}>
+                    <form className={style.contactForm} onSubmit={formik.handleSubmit}>
                         <div className={style.field}>
-                            <label htmlFor="contactFormUserName">Enter your name*</label>
-                            <input type="text" name={"username"} id={"contactFormUserName"}/>
+                            <label
+                                htmlFor="contactFormUserName">{formik.touched.name && formik.errors.name ?
+                                <span style={{color: 'red'}}>{formik.errors.name}</span> : 'Enter your name*'}</label>
+                            <input type="text" id={"contactFormUserName"} {...formik.getFieldProps('name')}/>
                         </div>
                         <div className={style.field}>
-                            <label htmlFor="contactFormUserEmail">Email</label>
-                            <input type="text" name={"useremail"} id={"contactFormUserEmail"}/>
+                            <label htmlFor="contactFormUserEmail">
+                                {formik.touched.email && formik.errors.email ?
+                                    <span style={{color: 'red'}}>{formik.errors.email}</span> : 'Enter your Email*'}
+                            </label>
+                            <input type="text" name="email" id={"contactFormUserEmail"} {...formik.getFieldProps('email')}/>
+                            {/*<ErrorMessage name="email" />*/}
                         </div>
                         <div className={`${style.field} ${style.message}`}>
-                            <label htmlFor="contactFormUserMessage">Message</label>
-                            <textarea name="textarea" id="contactFormUserMessage" cols={30} rows={6}></textarea>
+                            <label htmlFor="contactFormUserMessage">
+                                {formik.touched.text && formik.errors.text ?
+                                    <span style={{color: 'red'}}>{formik.errors.text}</span> : 'Enter your Message*'}
+                            </label>
+                            <textarea id="contactFormUserMessage" cols={30}
+                                      rows={6} {...formik.getFieldProps('text')}></textarea>
                         </div>
                         <div className={style.field}>
-                           <button className={style.contactFormBtn}>Send Mail</button>
+                            <button className={style.contactFormBtn} type={"submit"}>Send Mail</button>
                         </div>
                     </form>
                 </div>
