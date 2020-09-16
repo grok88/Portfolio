2b5305d4aa8c69e664246c95a83392f87423fa3e
Alex <grok88@tut.by>
2020-09-16 21:16:41 +0300

    added main block

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,17 +5,18 @@ import styleContainer from '../common/styles/Container.module.scss'
 const Main = () => {
     return (
         <div id='header' className={style.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={styleContainer.container + ' ' + style.test}>
                 <div className={style.info}>
-                    <span>Hi There</span>
-                    <h1><span className={style.subtitle}>I Am Alex Gor</span></h1>
+                    <span className={style.welcome}>Hi There !</span>
+                    <h1 className={style.title}>I'm<span className={style.subtitle}> Alexander Goryacko</span></h1>
                     <p>Frontend Developer</p>
                 </div>
                 <div className={style.photo}>
+                    {/*<img src={girl} alt=""/>*/}
                 </div>
             </div>
         </div>
     );
 }
 
-export default  Main;
+export default Main;
