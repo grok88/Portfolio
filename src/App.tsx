f51dcd996a940f2206a88008aba84b8a5d522e43
Alex <grok88@tut.by>
2020-11-15 20:40:54 +0300

    added photo

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,21 @@ import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import {Sidebar} from "./components/sidebar/Sidebar";
-import frontEnd from "./assets/images/frontend.jpg";
+// import frontEnd from "./assets/images/frontend.jpg";
+// import photo from "./assets/images/photo.jpg";
 
 const App = () => {
     return (
         <>
             <Sidebar/>
             <div className={style.mainContent}>
-                <div className={style.logo}>
-                    <h1>
-                        <a href="#">
-                            <img src={frontEnd} alt="logo"/>
-                        </a>
-                    </h1>
-                </div>
+                {/*<div className={style.logo}>*/}
+                {/*    <h1>*/}
+                {/*        <a href="#">*/}
+                {/*            <img src={photo} alt="logo"/>*/}
+                {/*        </a>*/}
+                {/*    </h1>*/}
+                {/*</div>*/}
                 <Main/>
                 <Skills/>
                 <Works/>
